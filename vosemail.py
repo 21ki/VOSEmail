ad8cb9f67f942e771b9bc6475fec3c2a8f4aba39
learnin9 <learnin9@gmail.com>
2016-02-20 11:54:23 +0800

    Update vosemail.py

--- vosemail.py
+++ vosemail.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 #encoding=utf-8
+import time
 import sys
 import MySQLdb
 import smtplib
@@ -15,23 +16,24 @@ MYSQL_SERVER="xxxxxx"
 MYSQL_USER="xxxx"
 MYSQL_PASS="xxxxx"
 DATABASE="xxxxxx"
-TITLE="你今天的话费消费记录"
+DATE=time.strftime('%Y-%m-%d',time.localtime(time.time()))
+TITLE='您'+str(DATE)+'时间VOS的话费消费记录'
 def send_mail(to_list,sub,content):  #to_list：收件人；sub：主题；content：邮件内容
     me="Email_VOS"+"<"+mail_user+"@"+mail_postfix+">"   #这里的hello可以任意设置，收到信后，将按照设置显示
     msg = MIMEText(content,_subtype='html',_charset='utf-8')    #创建一个实例，这里设置为html格式邮件
     msg['Subject'] = sub    #设置主题
-    msg['From'] = me  
-    msg['To'] = ";".join(to_list)  
-    try:  
-        smtpsend = smtplib.SMTP()  
+    msg['From'] = me
+    msg['To'] = ";".join(to_list)
+    try:
+        smtpsend = smtplib.SMTP()
         smtpsend.connect(mail_host)  #连接smtp服务器
         smtpsend.login(mail_user,mail_pass)  #登陆服务器
         smtpsend.sendmail(me, to_list, msg.as_string())  #发送邮件
-        smtpsend.close()  
-        return True  
-    except Exception, e:  
-        print str(e)  
-        return False  
+        smtpsend.close()
+        return True
+    except Exception, e:
+        print str(e)
+        return False
 try:
         db = MySQLdb.connect(MYSQL_SERVER,MYSQL_USER,MYSQL_PASS,DATABASE,charset='utf8')
         SQL = "Select account,money,limitmoney,todayconsumption,memo from e_customer where memo!=''"
@@ -43,6 +45,8 @@ try:
                 MONEY = data["money"]
                 LIMITMONEY = data["limitmoney"]
                 TODAYCONSUMPTION = data["todayconsumption"]
+                TITLE=str(ACCOUNT)+'公司'+TITLE
+#                print TITLE
                 TEXT= '亲爱的'+str(ACCOUNT)+'公司'+','+'您今天消费了'+str(TODAYCONSUMPTION)+','+'目前余额为'+str(MONEY)+','+'您的透支限度为'+str(LIMITMONEY)
                 mailto_list = data["memo"]  #从数据组字典中的memo里取得email地址
                 if __name__ == '__main__':
