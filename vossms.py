fa2aa2d857c7de204a69fae19cdcc201b64b7034
learnin9 <learnin9@gmail.com>
2016-06-16 21:26:05 +0800

    Update vossms.py

--- vossms.py
+++ vossms.py
@@ -2,27 +2,31 @@
 #encoding=utf-8
 import time
 import sys
-import MySQLdb   #需要自己下载该python库
-import top.api   #需要下载大鱼的python sdk http://download.taobaocdn.com/freedom/38879/compress/sdk-python-2016-06-07.zip
+import MySQLdb
+import top.api
 import json
 reload(sys)
 sys.setdefaultencoding('utf-8')
-MYSQL_SERVER=""  #MYSQL服务器连接
-MYSQL_USER=""    #MYSQL账号
-MYSQL_PASS=""    #Mysql连接密码
+MYSQL_SERVER=""
+MYSQL_USER=""
+MYSQL_PASS=""
 DATABASE="vosdb"
 DATE=time.strftime('%Y-%m-%d',time.localtime(time.time()))
 TITLE='您的'+str(DATE)+'时间VOS的话费消费记录'
-appkey = ""      #需要你的大鱼的认证appkey
-secret = ""      #需要你申请的大鱼的认证密码
+appkey = ""
+secret = ""
 partner_id = "taobao-sdk-python-20160607"
 host = "http://gw.api.taobao.com/router/rest"
 port = "80"
+
+req_voice = top.api.AlibabaAliqinFcVoiceNumSinglecallRequest()
+req_voice.set_app_info(top.appinfo(appkey,secret))
 req = top.api.AlibabaAliqinFcSmsNumSendRequest()
 req.set_app_info(top.appinfo(appkey,secret))
 try:
         db = MySQLdb.connect(MYSQL_SERVER,MYSQL_USER,MYSQL_PASS,DATABASE,charset='utf8')
         SQL = "Select account,money,limitmoney,todayconsumption,memo from e_customer where memo!=''"
+        SQL_NOMONEY = "select account,memo from e_customer where money<=0 and memo!=''"
         cursor = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
         cursor.execute(SQL)
         for data in cursor.fetchall():
@@ -38,20 +42,35 @@ try:
                 print "限额",LIMITMONEY
                 print "当日消费",TODAYCONSUMPTION
                 print "电话",PHONE
-        #       req.format = "json"
                 req.extend = ""
                 req.sms_type = "normal"
-                req.sms_free_sign_name = "大鱼测试"
+                req.sms_free_sign_name = "XXXX"
                 params = {"name":ACCOUNT,"m":MONEY,"l":LIMITMONEY,"t":TODAYCONSUMPTION}
                 req.sms_param = json.dumps(params)
-#               print req.sms_param
                 req.rec_num = PHONE
-                req.sms_template_code = ""    #需要sms模板ID
+                req.sms_template_code = ""
                 try :
                         resp = req.getResponse()
                         print (resp)
                 except Exception,e:
                         print (e)
+        cursor.execute(SQL_NOMONEY)
+        for data in cursor.fetchall():
+#               print data
+                result = str(data).decode('raw_unicode_escape')
+#               ACCOUNT = str(data["account"])
+                PHONE = str(data["memo"])
+#               print "账号",ACCOUNT
+                print "电话",PHONE
+                req_voice.extend = ""
+                req_voice.called_num = PHONE
+                req_voice.called_show_num = "4001003782"
+                req_voice.voice_code = "xxxxxxxxxx.wav"
+                try:
+                        resp= req_voice.getResponse()
+                        print(resp)
+                except Exception,e:
+                        print(e)
 except MySQLdb.Error,e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
 cursor.close()
